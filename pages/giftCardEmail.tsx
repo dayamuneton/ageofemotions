ce688ff7a71f1a8dd78db6cc2e41dff836531c01
agredo-alejo <agredoalejo@gmail.com>
2022-12-25 19:06:25 -0500

    deleted giftcard email page

--- pages/giftCardEmail.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import GiftCardEmail from "../templates/giftcardEmail";
-
-function GiftCardEmailt() {
-   return <GiftCardEmail name={"Alejandro"} />;
-   // return <p>hola</p>;
-}
-
-export default GiftCardEmailt;
