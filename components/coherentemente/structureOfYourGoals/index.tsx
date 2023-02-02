f6b2f5b18008243e0e89cd98d08e8f192d3d7ac4
agredo-alejo <agredoalejo@gmail.com>
2023-02-02 00:28:27 -0500

    change photoURL to mente_fisica card

--- components/coherentemente/structureOfYourGoals/index.tsx
+++ components/coherentemente/structureOfYourGoals/index.tsx
@@ -16,8 +16,7 @@ export const pdfs = [
    },
    {
       title: "Estructura De Tus Metas Con Una Mente Física",
-      photoURL:
-         "https://firebasestorage.googleapis.com/v0/b/amayliberate.appspot.com/o/images%2Fcover%20estructura%20mente%20fisica.png?alt=media&token=23c2361b-a231-4e45-806e-9e9f148abcb2",
+      photoURL: "/MenteFisica.png",
       text: "Si quieres ser libre de decidir como quieres vivir o ver la vida, para lograr resultados en coherencia con lo que te propones,  aprende más sobre tu cuerpo unificado a tu mente.",
       buttonText: "OBTÉN ACCESO INMEDIATO",
       link: `/turegalo/mente_fisica`,
@@ -28,7 +27,7 @@ export const pdfs = [
    },
    {
       title: "AGE OF EMOTIONS",
-      photoURL: "/CoverMenteFisica.png",
+      photoURL: "/MenteFisica.png",
       subTitle: "10 Prácticas Para Ganar Percepción Corporal De Tus Emociones.",
       text: "No percibir tu cuerpo como parte de tu consciencia, ha hecho que sigas gestionando emociones como si vivieras en otra época y te ha llevado a tener muchas confusiones, dolor, miedo y estancamiento en varios aspectos de tu vida.",
       buttonText: "SABER MÁS...",
